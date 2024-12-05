c2a3b7b8fa6040cc1f701e7d546ae6525062c0bc
Ramesh92340 <rameshpilli1428@gmail.com>
2024-12-05 09:32:55 +0530

    navbar  updated

--- index.php
+++ index.php
@@ -64,183 +64,177 @@
      <!-- ======= Header ======= -->
 
 
-     <header id="header" class="header_bg">
-         <div class="container d-flex align-items-center onlypad_index  ">
-             <div class="logo-text-container d-flex align-items-center d-lg-none" style="z-index: 999;">
-                 <a href="index.php">
-                     <img src="assets/img/vision/large_icon.png" class="img-fluid  index_logo_sm_md" alt="">
-                 </a>
-             </div>
+   
+  <header id="header">
+    <div class="container d-flex align-items-center onlypad  ">
+      <div class="logo-text-container d-flex align-items-center" style="z-index: 999;">
+        <a href="index.php">
+          <img src="assets/img/vision/large_icon.png" class="img-fluid nav_logo_image " alt="">
+        </a>
+      </div>
 
-             <nav id="navbar" class="  navbar_empyt navbar order-lg-0">
-                 <i class="bi bi-list mobile-nav-toggle"></i>
-                 <ul>
-                     <li class="nav-item active"><a class="nav-link scrollto" href="index.php">Home</a></li>
-                     <li><a class="nav-link scrollto" href="about.php">About</a></li>
-                     <li class="unique-nav-item " style="--i: 1.1s">
-              <a href="treatment.php" class="unique-nav-link"
-                >Treatments<i class="fas fa-caret-down"></i
-              ></a>
-              <div class="unique-dropdown" >
-                <ul class="unique-dropdown-list  new_nav_bg"  >
-                 
-                   
-                  <li class="unique-dropdown-item">
-                    <a href="#" class="unique-dropdown-link"
-                      >Pain Relief<i class="fas fa-caret-down"></i
-                    ></a>
-                    <div class="unique-dropdown second-level  nav_div_mrg nav_div_mrg_one"  >
-                      <ul class="unique-dropdown-list inside_nav_drop_down">
-                        <li class="unique-dropdown-item">
-                          <a href="root.php" class="unique-dropdown-link">Root Canal Treatment</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="tooth.php" class="unique-dropdown-link">  Wisdom Tooth Removel</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="jaw.php" class="unique-dropdown-link"> Jaw Pain / Correction</a>
-                        </li>
-                        
-                        <div class="unique-arrow"></div>
-                      </ul>
-                    </div>
-                  </li>
-                  <li class="unique-dropdown-item">
-                    <a href="#" class="unique-dropdown-link"
-                      >Teeth Replacement<i class="fas fa-caret-down"></i
-                    ></a>
-                    <div class="unique-dropdown second-level  nav_div_mrg nav_div_mrg_two">
-                      <ul class="unique-dropdown-list inside_nav_drop_down">
-                        <li class="unique-dropdown-item">
-                          <a href="dentures.php" class="unique-dropdown-link">Dentures</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="crowns.php" class="unique-dropdown-link">Crowns & Bridges</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="implants.php" class="unique-dropdown-link">Dental Implants</a>
-                        </li>
-                        
-                        <div class="unique-arrow"></div>
-                      </ul>
-                    </div>
-                  </li>
-                  <li class="unique-dropdown-item">
-                    <a href="#" class="unique-dropdown-link"
-                      >Laminate  Veneers<i class="fas fa-caret-down"></i
-                    ></a>
-                    <div class="unique-dropdown second-level  nav_div_mrg nav_div_mrg_three">
-                      <ul class="unique-dropdown-list inside_nav_drop_down">
-                        <li class="unique-dropdown-item">
-                          <a href="aligners.php" class="unique-dropdown-link"> Clear Aligners</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="braces.php" class="unique-dropdown-link">Braces</a>
-                        </li>
-                         <li class="unique-dropdown-item"> 
-                          <a href="cosmetic.php" class="unique-dropdown-link">Laminate  Veneers</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="smile.php" class="unique-dropdown-link"> Smile Designing</a>
-                        </li>
-                       
-                        <div class="unique-arrow"></div>
-                      </ul>
-                    </div>
-                  </li>
+      <nav id="navbar" class="navbar order-lg-0">
+        <i class="bi bi-list mobile-nav-toggle"></i>
+        <ul>
+          <li><a class="nav-link scrollto " href="index.php">Home</a></li>
+          <li class="nav-item active">
+            <a class="nav-link scrollto" href="about.php">About</a>
+          </li>
 
-                  <li class="unique-dropdown-item">
-                    <a href="#" class="unique-dropdown-link"
-                      >Laser Veda<i class="fas fa-caret-down"></i
-                    ></a>
-                    <div class="unique-dropdown second-level  nav_div_mrg nav_div_mrg_four">
-                      <ul class="unique-dropdown-list inside_nav_drop_down">
-                        <li class="unique-dropdown-item">
-                          <a href="laserTooth.php" class="unique-dropdown-link"> Laser assisted Teeth Cleaning</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="gum" class="unique-dropdown-link">  Gum Depigmentation</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="laserTeeth.php" class="unique-dropdown-link">  Laser Teeth Whitening</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="laserGum.php" class="unique-dropdown-link"> Laser Gum Surgeries</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="mouth.php" class="unique-dropdown-link"> Mouth Ulcers</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="lasion.php" class="unique-dropdown-link">  Precancerous Lesion in Mouth</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="laserCrown.php" class="unique-dropdown-link"> Laser Crown Lengthening</a>
-                        </li>
-                        
-                        
-                        <div class="unique-arrow"></div>
-                      </ul>
-                    </div>
-                  </li>
+          <li class="unique-nav-item " style="--i: 1.1s">
+            <a href="treatment.php" class="unique-nav-link">Treatments<i class="fas fa-caret-down"></i></a>
+            <div class="unique-dropdown">
+              <ul class="unique-dropdown-list  new_nav_bg">
 
-                  
-                  <li class="unique-dropdown-item">
-                    <a href="#" class="unique-dropdown-link"
-                      >   Child Dentistry<i class="fas fa-caret-down"></i
-                    ></a>
-                    <div class="unique-dropdown second-level nav_div_mrg nav_div_mrg_five" >
-                      <ul class="unique-dropdown-list inside_nav_drop_down">
-                        <li class="unique-dropdown-item">
-                          <a href="pediatric.php" class="unique-dropdown-link">Pediatric Dentist</a>
-                        </li>
-                        <li class="unique-dropdown-item">
-                          <a href="paedodontist.php" class="unique-dropdown-link">Paedodontist Doctors</a>
-                        </li>
-                     
-                        
-                        <div class="unique-arrow"></div>
-                      </ul>
-                    </div>
-                  </li>
 
-                   
-                  <div class="unique-arrow"></div>
-                </ul>
-              </div>
-            </li>
-                     <li><a class="nav-link scrollto" href="equipment.php">equipment</a></li>
+                <li class="unique-dropdown-item">
+                  <a href="#" class="unique-dropdown-link">Pain Relief<i class="fas fa-caret-down"></i></a>
+                  <div class="unique-dropdown second-level  nav_div_mrg nav_div_mrg_one">
+                    <ul class="unique-dropdown-list inside_nav_drop_down">
+                      <li class="unique-dropdown-item">
+                        <a href="root.php" class="unique-dropdown-link">Root Canal Treatment</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="tooth.php" class="unique-dropdown-link"> Wisdom Tooth Removel</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="jaw.php" class="unique-dropdown-link"> Jaw Pain / Correction</a>
+                      </li>
 
-                     <li><a class="nav-link" href="blogs.php">Blogs</a></li>
-                     <li><a class="nav-link scrollto" href="testimonials.php">Reviews</a></li>
-                     <li><a class="nav-link scrollto" href="gallery.php">Smile Gallery</a></li>
+                      <div class="unique-arrow"></div>
+                    </ul>
+                  </div>
+                </li>
+                <li class="unique-dropdown-item">
+                  <a href="#" class="unique-dropdown-link">Teeth Replacement<i class="fas fa-caret-down"></i></a>
+                  <div class="unique-dropdown second-level  nav_div_mrg nav_div_mrg_two">
+                    <ul class="unique-dropdown-list inside_nav_drop_down">
+                      <li class="unique-dropdown-item">
+                        <a href="dentures.php" class="unique-dropdown-link">Dentures</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="crowns.php" class="unique-dropdown-link">Crowns & Bridges</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="implants.php" class="unique-dropdown-link">Dental Implants</a>
+                      </li>
 
-                     <li><a href="appointment.php" class="appointment-btn scrollto d-lg-none" style="z-index: 999;">
-                             Appointment
-                         </a></li>
-                 </ul>
-             </nav>
+                      <div class="unique-arrow"></div>
+                    </ul>
+                  </div>
+                </li>
+                <li class="unique-dropdown-item">
+                  <a href="#" class="unique-dropdown-link">Laminate Veneers<i class="fas fa-caret-down"></i></a>
+                  <div class="unique-dropdown second-level  nav_div_mrg nav_div_mrg_three">
+                    <ul class="unique-dropdown-list inside_nav_drop_down">
+                      <li class="unique-dropdown-item">
+                        <a href="aligners.php" class="unique-dropdown-link"> Clear Aligners</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="braces.php" class="unique-dropdown-link">Braces</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="cosmetic.php" class="unique-dropdown-link">Laminate Veneers</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="smile.php" class="unique-dropdown-link"> Smile Designing</a>
+                      </li>
 
-             <a href="appointment.php" class="appointment_btn_index scrollto d-none d-lg-block" style="z-index: 999;">
-                 Appointment
-             </a>
-         </div>
+                      <div class="unique-arrow"></div>
+                    </ul>
+                  </div>
+                </li>
 
-         <script>
-             // jQuery needed to manage hover and click behavior
-             $(document).ready(function() {
-                 // Ensure that the dropdown opens on hover
-                 $('#servicesDropdown').hover(function() {
-                     $(this).dropdown('toggle');
-                 });
+                <li class="unique-dropdown-item">
+                  <a href="#" class="unique-dropdown-link">Laser Veda<i class="fas fa-caret-down"></i></a>
+                  <div class="unique-dropdown second-level  nav_div_mrg nav_div_mrg_four">
+                    <ul class="unique-dropdown-list inside_nav_drop_down">
+                      <li class="unique-dropdown-item">
+                        <a href="laserTooth.php" class="unique-dropdown-link"> Laser assisted Teeth Cleaning</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="gum" class="unique-dropdown-link"> Gum Depigmentation</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="laserTeeth.php" class="unique-dropdown-link"> Laser Teeth Whitening</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="laserGum.php" class="unique-dropdown-link"> Laser Gum Surgeries</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="mouth.php" class="unique-dropdown-link"> Mouth Ulcers</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="lasion.php" class="unique-dropdown-link"> Precancerous Lesion in Mouth</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="laserCrown.php" class="unique-dropdown-link"> Laser Crown Lengthening</a>
+                      </li>
 
-                 // Make sure the dropdown also works on click
-                 $('#servicesDropdown').click(function(e) {
-                     e.stopPropagation(); // Prevents the click from closing the dropdown immediately
-                     window.location.href = $(this).attr('href'); // Redirect to the link
-                 });
-             });
-         </script>
-     </header>
+
+                      <div class="unique-arrow"></div>
+                    </ul>
+                  </div>
+                </li>
+
+
+                <li class="unique-dropdown-item">
+                  <a href="#" class="unique-dropdown-link"> Child Dentistry<i class="fas fa-caret-down"></i></a>
+                  <div class="unique-dropdown second-level nav_div_mrg nav_div_mrg_five">
+                    <ul class="unique-dropdown-list inside_nav_drop_down">
+                      <li class="unique-dropdown-item">
+                        <a href="pediatric.php" class="unique-dropdown-link">Pediatric Dentist</a>
+                      </li>
+                      <li class="unique-dropdown-item">
+                        <a href="paedodontist.php" class="unique-dropdown-link">Paedodontist Doctors</a>
+                      </li>
+
+
+                      <div class="unique-arrow"></div>
+                    </ul>
+                  </div>
+                </li>
+
+
+                <div class="unique-arrow"></div>
+              </ul>
+            </div>
+          </li>
+
+
+          <li><a class="nav-link scrollto" href="equipment.php">equipment</a></li>
+          <li><a class="nav-link" href="blogs.php">Blogs</a></li>
+          <li><a class="nav-link scrollto" href="testimonials.php">Reviews</a></li>
+          <li><a class="nav-link scrollto" href="gallery.php">Smile Gallery</a></li>
+
+          <li><a href="appointment.php" class="appointment-btn scrollto d-lg-none" style="z-index: 999;">
+              Appointment
+            </a></li>
+        </ul>
+      </nav>
+
+      <a href="appointment.php" class="appointment-btn scrollto d-none d-lg-block" style="z-index: 999;">
+        Appointment
+      </a>
+    </div>
+
+
+    <script>
+      // jQuery needed to manage hover and click behavior
+      $(document).ready(function() {
+        // Ensure that the dropdown opens on hover
+        $('#servicesDropdown').hover(function() {
+          $(this).dropdown('toggle');
+        });
+
+        // Make sure the dropdown also works on click
+        $('#servicesDropdown').click(function(e) {
+          e.stopPropagation(); // Prevents the click from closing the dropdown immediately
+          window.location.href = $(this).attr('href'); // Redirect to the link
+        });
+      });
+    </script>
+  </header>
 
 
      <section class="banner_bg_color">
@@ -1031,8 +1025,6 @@
      </section>
 
 
-
-
      <section class="lady_doc_sec">
          <div class="container">
              <div class="row">
@@ -1073,10 +1065,7 @@
              <div class="section-title text-center">
 
                  <h2 class="ask_heading mb-5">Blogs & articles </h2>
-
-
-
-
+ 
              </div>
          </div>
 
