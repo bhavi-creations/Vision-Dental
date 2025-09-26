4698068491fe85eba600deb19e2cb0b3b64f6231
manimalladi <manimalladoi05@gmail.com>
2025-09-26 22:52:13 +0530

    increase the font size

--- service_detsils.php
+++ service_detsils.php
@@ -411,7 +411,7 @@ if ($blog_id > 0) {
 
                                 <!-- Main Content -->
                                 <?php if (!empty($blog['main_content'])): ?>
-                                    <div class="ul-service-details-descr">
+                                    <div class="ul-service-details-descr" style="font-size:18px">
                                         <?php
                                         // Allow basic formatting tags: p, br, ul, ol, li, b, i, strong, em
                                         echo strip_tags($blog['main_content'], '<p><br><ul><ol><li><b><i><strong><em>');
@@ -952,7 +952,7 @@ if ($blog_id > 0) {
 
                                 <!-- Main Content -->
                                 <?php if (!empty($blog['main_content'])): ?>
-                                    <div class="ul-service-details-descr">
+                                    <div class="ul-service-details-descr"  style="font-size:18px">
                                         <?php
                                         // Allow basic formatting tags: p, br, ul, ol, li, b, i, strong, em
                                         echo strip_tags($blog['main_content'], '<p><br><ul><ol><li><b><i><strong><em>');
@@ -1020,7 +1020,7 @@ if ($blog_id > 0) {
 
                                 <!-- Full Content -->
                                 <?php if (!empty($blog['full_content'])): ?>
-                                    <div class="ul-service-details-full-content mt-3">
+                                    <div class="ul-service-details-full-content mt-3"  style="font-size:18px">
                                         <?php
                                         // Allow basic formatting tags: p, br, ul, ol, li, b, i, strong, em
                                         echo strip_tags($blog['full_content'], '<p><br><ul><ol><li><b><i><strong><em>');
